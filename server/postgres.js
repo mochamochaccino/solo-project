684c4450acdccb80718d105b5f0e556f5dfe9235
Michael <156242839+mochamochaccino@users.noreply.github.com>
2024-07-30 21:10:02 -0500

    final commit 7/30

--- server/postgres.js
+++ server/postgres.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const db = {
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+    user: 'postgres',
+    host: '192.168.1.134',
+    database: 'postgres',
+    password: '',
+    port: 5432
 }
 
 const pool = new Pool(db);
